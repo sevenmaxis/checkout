6bb49532492b9bac2cc9b22623baef8031d5854a
Sergey Gopkalo <Sergey.Gopkalo@gmail.com>
2013-04-28 06:38:20 +0300

    fix code style

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -23,8 +23,8 @@ class Strawberry < Rule
   PRICE = 450
   
   def apply(items)
-    if items.count{ |item| item.code == 'SR'} >= QUANTITY
-      items.each { |item| item.price = PRICE if item.code == 'SR' }
+    if items.count{ |item| item.code == 'SR' } >= QUANTITY
+      items.each{ |item| item.price = PRICE if item.code == 'SR' }
     end
     items
   end
